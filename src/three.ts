765a26c3d1b674c05594922e4d0feb33d39866d8
Will Fenton <willfenton14@gmail.com>
2020-05-02 02:00:54 -0600

    on second thought, decreased terrain size

--- src/three.ts
+++ src/three.ts
@@ -123,8 +123,8 @@ const globalFrequency = 10;
 const elevationExponent = 2.25;
 const moistureExponent = 1.35;
 
-const width = 2048;
-const height = 2048;
+const width = 1024;
+const height = 1024;
 
 let elevationNoise: Function;
 let moistureNoise: Function;
